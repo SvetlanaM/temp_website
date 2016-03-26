2cc96b8700e657fb7d6428a760e91ddb11852c38
Svetlana <svetlana@margetova.eu>
2016-03-26 17:35:17 +0100

    736px

--- index.php
+++ index.php
@@ -214,7 +214,7 @@
                       </div>
                     </div>
                     <div class="row footer-box">
-                      <div class="col-sm-4 col-xs-12 col-md-3 col-lg-2">
+                      <div class="col-sm-4 col-xs-4 col-md-3 col-lg-2">
                         <jdoc:include type="modules" name="kontakt" />
                       </div>
                       <div class="col-sm-8 col-xs-8 col-md-3 col-lg-2">
