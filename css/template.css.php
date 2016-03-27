8b40f855f927df22545fedeb601b830fcf84d63b
Svetlana <svetlana@margetova.eu>
2016-03-27 21:52:24 +0200

    modal window optimalization

--- css/template.css.php
+++ css/template.css.php
@@ -24,9 +24,9 @@ header($ExpStr);
 function compress($buffer) {
 	/* remove comments */
 	$buffer = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $buffer);
-	/* remove tabs, spaces, new lines, etc. */        
+	/* remove tabs, spaces, new lines, etc. */
 	$buffer = str_replace(array("\r\n","\r","\n","\t",'  ','    ','    '),'',$buffer);
-	/* remove unnecessary spaces */        
+	/* remove unnecessary spaces */
 	$buffer = str_replace('{ ', '{', $buffer);
 	$buffer = str_replace(' }', '}', $buffer);
 	$buffer = str_replace('; ', ';', $buffer);
@@ -37,7 +37,7 @@ function compress($buffer) {
 	$buffer = str_replace(' ,', ',', $buffer);
 	$buffer = str_replace(' ;', ';', $buffer);
 	$buffer = str_replace(';}', '}', $buffer);
-	
+
 	return $buffer;
 }
 
